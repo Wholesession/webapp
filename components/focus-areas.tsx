ac21234ac4eb974fe9eaf002054bf61dcc3c5280
Joseph Olatunde <iamjosephtunde@gmail.com>
2026-01-11 05:05:34 +0100

    fixed focus areas

--- components/focus-areas.tsx
+++ components/focus-areas.tsx
@@ -34,7 +34,7 @@ export function FocusAreas() {
                             </div>
 
                             <div className="absolute inset-0 flex flex-col justify-end p-6 z-10">
-                                <span className="text-3xl font-bold text-black group-hover:scale-105 transition-transform duration-300">
+                                <span className="text-2xl sm:text-3xl font-bold text-black group-hover:scale-105 transition-transform duration-300">
                                     {category.name}
                                 </span>
                             </div>
